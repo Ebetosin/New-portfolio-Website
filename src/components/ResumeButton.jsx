a1a2dfb07130a5f822174eb072e556fb2eba6c37
Ebetosin <ebetosin@gmail.com>
2026-01-05 16:45:28 +0100

    cv changed to inside file

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -1,12 +1,29 @@
+// import { FiDownload } from 'react-icons/fi'
+
+// const ResumeButton = () => {
+//   return (
+//     <a
+//       href="https://drive.google.com/uc?export=download&id=1JJmizpLRBG8ZLX_VjtCXeWc-ZD3UW8q2"
+//       className="btn primary download-btn"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//     >
+//       Download CV
+//       <FiDownload className="download-icon" />
+//     </a>
+//   )
+// }
+
+// export default ResumeButton
+
 import { FiDownload } from 'react-icons/fi'
 
 const ResumeButton = () => {
   return (
     <a
-      href="https://drive.google.com/uc?export=download&id=1JJmizpLRBG8ZLX_VjtCXeWc-ZD3UW8q2"
+      href="/cv/Ebenezer_CV.pdf"
+      download
       className="btn primary download-btn"
-      target="_blank"
-      rel="noopener noreferrer"
     >
       Download CV
       <FiDownload className="download-icon" />
@@ -15,3 +32,4 @@ const ResumeButton = () => {
 }
 
 export default ResumeButton
+
